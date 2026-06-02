c258345a725c37f9a9ff25ece5cc948b6b5d4b48
LucasCr1ss <cristianolucas073@gmail.com>
2026-06-02 20:42:35 -0300

    aula pw2 02062026

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
+const path = require("path");
+const prisma = require("./config/prisma");
+const { PrismaClient } = require("@prisma/client");
+require("dotenv").config({ path: path.resolve(__dirname, ".env")  });
+
 const express = require("express");
 const clienteRoutes = require("./Routes/clienteRoutes");
 
